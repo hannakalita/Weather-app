3e79bc5850407f1bff20112f85dadd76c82faff3
hannakalita <annakalita.work@gmail.com>
2022-07-17 14:58:40 +0200

    Added js to show actual data

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-alert("Hello!");
+function showTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temp");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "105f5194b1bc6b92da7dc7390ee98c0f";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
