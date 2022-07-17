8bd7bf19a1b46d8b02e665c49d6898f9bb45f670
hannakalita <annakalita.work@gmail.com>
2022-07-17 17:08:34 +0200

    Upgreted everything

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
+function actualTime(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = days[date.getDay()];
+  return `Today is ${currentDay}, ${hours}:${minutes}`;
+}
 function showTemperature(response) {
-  console.log(response);
   let temperatureElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -11,7 +31,12 @@ function showTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
 }
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = actualTime(currentTime);
+let city = "Kyiv";
 let apiKey = "105f5194b1bc6b92da7dc7390ee98c0f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
